8515d19f7f22a0b8c5eedfe990c984557dcafb56
Jainendra Dwivedi <75388379+JainendraDwivedi@users.noreply.github.com>
2021-10-13 19:02:05 +0530

    adding html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+const path = require("path");
+
+const {
+  loginHTML,
+  homeHTML,
+  signupHTML,
+  bmiHTML,
+} = require("../controllers/html-controllers");
+
+router.get("/", loginHTML);
+
+router.get("/home", homeHTML);
+
+router.get("/signup", signupHTML);
+
+router.get("/bmi", bmiHTML);
+
+module.exports = router;
