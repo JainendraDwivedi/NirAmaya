15e57c640658892bb639390bc4b29a92d1b55808
Jainendra Dwivedi <75388379+JainendraDwivedi@users.noreply.github.com>
2021-10-13 13:24:04 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const { getUserById, getUser, updateUser,userPurchaseList } = require("../controllers/user");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
+router.param("userId", getUserById);
+
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList);
 module.exports = router;
