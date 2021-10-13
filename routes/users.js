a6cac295ef2a97242be99e4a1c97dd17bc366afe
Jainendra Dwivedi <75388379+JainendraDwivedi@users.noreply.github.com>
2021-10-13 13:30:28 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -10,3 +10,4 @@ router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
 router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList);
 module.exports = router;
+//
