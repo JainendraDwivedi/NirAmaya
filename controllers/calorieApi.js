a3c247cb3aa5762bbf1180ee0c44388e374ce7b6
Jainendra Dwivedi <75388379+JainendraDwivedi@users.noreply.github.com>
2021-10-19 15:12:17 +0530

    Ansh added calorie api controller

--- controllers/calorieApi.js
+++ controllers/calorieApi.js
@@ -1,66 +1,56 @@
-const https = require('https');
+const https = require("https");
 const bodyParser = require("body-parser");
-const request = require('request');
-
+const request = require("request");
 
 exports.getData = (req, res) => {
   var query = req.body.foodItem;
-//   https.get({
-//     url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query ,
-//     headers: {
-//       'X-Api-Key': 'NidnBMrMCDT8WCkcKqueQ7VtkrGnPdimy0pecOHp'
-//     },
-//   }, function(response) {
-//     console.log(response.statusCode);
 
-//     response.on("data", function (data){
-//       const fooditems = JSON.parse(data);
-//       console.log(fooditems);
-//     });
-//     res.render('home');
-// });
+  request.get(
+    {
+      url: "https://api.calorieninjas.com/v1/nutrition?query=" + query,
+      headers: {
+        "X-Api-Key": "NidnBMrMCDT8WCkcKqueQ7VtkrGnPdimy0pecOHp",
+      },
+    },
+    function (error, response, body) {
+      if (error) return console.error("Request failed:", error);
+      else if (response.statusCode != 200)
+        return console.error(
+          "Error:",
+          response.statusCode,
+          body.toString("utf8")
+        );
+      else {
+        const items = JSON.parse(body);
 
+        console.log(items.items[0].sugar_g);
 
+        const Name = items.items[0].name;
+        const Serving_Size = items.items[0].serving_size_g;
+        const Calories = items.items[0].calories;
+        const Total_Fat = items.items[0].fat_total_g;
+        const Saturated_Fat = items.items[0].fat_saturated_g;
+        const Cholesterol = items.items[0].cholesterol_mg;
+        const Sodium = items.items[0].sodium_mg;
+        const Carbohydrates = items.items[0].carbohydrates_total_g;
+        const Fiber = items.items[0].fiber_g;
+        const Sugar = items.items[0].sugar_g;
+        const Protein = items.items[0].protein_g;
 
-request.get({
-  url: 'https://api.calorieninjas.com/v1/nutrition?query='+query,
-  headers: {
-    'X-Api-Key': 'NidnBMrMCDT8WCkcKqueQ7VtkrGnPdimy0pecOHp'
-  },
-}, function(error, response, body) {
-  if(error) return console.error('Request failed:', error);
-  else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
-  else {
-    const items = JSON.parse(body);
-
-    console.log(items.items[0].sugar_g);
-
-    const Name = items.items[0].name;
-    const Serving_Size = items.items[0].serving_size_g;
-    const Calories = items.items[0].calories;
-    const Total_Fat = items.items[0].fat_total_g;
-    const Saturated_Fat = items.items[0].fat_saturated_g;
-    const Cholesterol = items.items[0].cholesterol_mg;
-    const Sodium = items.items[0].sodium_mg;
-    const Carbohydrates = items.items[0].carbohydrates_total_g;
-    const Fiber = items.items[0].fiber_g;
-    const Sugar = items.items[0].sugar_g;
-    const Protein = items.items[0].protein_g;
-
-    res.render("home",{
-      Name : Name,
-      Serving_Size : Serving_Size,
-      Calories : Calories,
-      Total_Fat : Total_Fat,
-      Saturated_Fat : Saturated_Fat,
-      Cholesterol : Cholesterol ,
-      Sodium :Sodium,
-      Carbohydrates :Carbohydrates,
-      Fiber :Fiber,
-      Sugar :Sugar,
-      Protein : Protein 
-    })
-    
-  }
-});
-}
+        res.render("calorieApi", {
+          Name: Name,
+          Serving_Size: Serving_Size,
+          Calories: Calories,
+          Total_Fat: Total_Fat,
+          Saturated_Fat: Saturated_Fat,
+          Cholesterol: Cholesterol,
+          Sodium: Sodium,
+          Carbohydrates: Carbohydrates,
+          Fiber: Fiber,
+          Sugar: Sugar,
+          Protein: Protein,
+        });
+      }
+    }
+  );
+};
